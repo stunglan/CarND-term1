7c69d4a83a2d9f3b12dfb3c96d82abe6a20d617b
Knut Sebastian Tungland <stunglan@gmail.com>
2017-01-14 15:17:12 +0000

    load all data

--- behavior-cloning/model.py
+++ behavior-cloning/model.py
@@ -5,9 +5,10 @@
 
 
 from keras.models import Sequential
-from keras.layers.core import Dense, Dropout, Activation, Flatten, Reshape
-from keras.layers.convolutional import Convolution1D, Convolution2D, MaxPooling2D
+from keras.layers.core import Dense, Dropout, Activation, Flatten, Reshape, Lambda,Dropout
+from keras.layers.convolutional import Convolution2D
 from keras.utils import np_utils
+from keras.layers.advanced_activations import ELU
 
 import cv2
 import numpy as np
@@ -17,6 +18,7 @@ import tensorflow as tf
 tf.python.control_flow_ops = tf
 
 data_dir = './data/udacity_data/'
+test_data_dir = './data/test_data/'
 rows = 80
 cols = 160
 channels = 3
@@ -31,51 +33,32 @@ def read_image(filename):
     newimage = cv2.cvtColor(newimage,cv2.COLOR_BGR2RGB)
     return newimage
 
-def process_line(path,line):
-    line = line.split(',')
-    #center,left,right,steering,throttle,brake,speed
 
-    #print('image filename: ', line[0], ' steering angle: ',line[3])
-    image = read_image(path + '/' + line[0])
-    #print(' image shape: ', image.shape)
-    return image, line[3]
-
-
-def generate_arrays_from_file(path):
-    X = np.zeros((batchSize, cols, rows,channels))
-    Y = np.zeros((batchSize, 1))
-    batchIndex = 0
-    while 1:
-        l = 0
-        #print('csv filename: ', path)
-        f = open(path + '/driving_log.csv')
-        for line in f:
-            #print('X shape',X[batchIndex].shape)
-            X[batchIndex], Y[batchIndex] = process_line(path,line)
-            m = (batchIndex+1)%batchSize 
-            #print(' line: {}, modulus: {}'.format(l,m))
-            if (m== 0):
-                batchIndex = 0
-                yield X,Y
-            batchIndex += 1
-            l += 1
-        f.close()
-
-
-
-def test_generator():
-    """Dummy routine to test the generator"""
-    a
-    c   = 0
-    for X,y in generate_arrays_from_file('./data'):
-        c += 1
-    print(c)
-
-    exit()
-
-#test_generator()
-
-# TODO: create the model
+def load_data(X,y,path):
+    side_camera_adjustment = 0.2
+    i = 0
+    f = open(path + '/driving_log.csv')
+    for line in f:
+        line = line.split(',')
+        X.append(read_image(path+'/'+line[0].strip()))
+        i += 1
+        angle = float(line[3])
+        y.append(angle)
+        #left camera
+        if line[1].strip():
+            X.append(read_image(path+'/'+line[1].strip()))
+            i += 1
+            angle += side_camera_adjustment
+            y.append(angle)
+        #right camera
+        if line[1].strip():
+            X.append(read_image(path+'/'+line[2].strip()))
+            i += 1
+            angle -= side_camera_adjustment
+            y.append(angle)
+    f.close()
+    return X,y
+        
 
 def create_nvidia_model():
     """The nividia model 
@@ -93,6 +76,8 @@ def create_nvidia_model():
     # ELU http://image-net.org/challenges/posters/JKU_EN_RGB_Schwarz_poster.pdf
     act = ELU
 
+
+    
     # 8x8 convolution (kernel) with 4x4 stride over 16 output filters
     model.add(Convolution2D(16, 8, 8, subsample=(4, 4), border_mode="same"))
     model.add(act())
@@ -113,46 +98,64 @@ def create_nvidia_model():
     model.add(act())
     # Fully connected layer with one output dimension (representing the speed).
     model.add(Dense(1))
-
+    
+    model.compile('adam', 'mean_squared_error', ['accuracy'])
     return model
 
-def create_dummy_model():
-    """Create a dummy model to rapidly test the surounding code
-    """
-    model = Sequential()
-    model.add(Flatten(input_shape=(image_shape[1], image_shape[0], 3)))
-    model.add(Dense(128))
-    model.add(Activation('relu'))
-    model.add(Dense(1))
-    return model
-
-mymodel = create_dummy_model()
 
 
 
-# TODO: train the model
+if __name__ == "__main__":
 
-mymodel.compile('adam', 'mean_squared_error', ['accuracy'])
+    X = []
+    y = []
+    X,y = load_data(X,y,data_dir)
+    X = np.array(X).astype('float32')
+    y = np.array(y).astype('float32')
+    print('X.shape: ',X.shape)
+    print('Y.shape: ',y.shape)
 
-# samples_per_epoch and batch size must be aligned
-history = mymodel.fit_generator(generate_arrays_from_file(data_dir),  samples_per_epoch=256*100, nb_epoch=10, verbose=1)
+    
 
-
-print(history.history)
-# TODO: check the accuracy
-
-# export the model as required in the assignment
-json_string = mymodel.to_json()
-with open('model.json', 'w') as outfile:
-    json.dump(json_string, outfile)
-
-# export the weights as required in the assignment
-mymodel.save_weights('model.h5')
+    mymodel = create_nvidia_model()
 
 
-# Explicitly reap session to avoid an AttributeError sometimes thrown by
-# TensorFlow on shutdown. See:
-# https://github.com/tensorflow/tensorflow/issues/3388
-from keras import backend as K
-K.clear_session()
 
+    # TODO: train the model
+    # samples_per_epoch and batch size must be aligned
+    history = mymodel.fit(X,y,nb_epoch=5, shuffle=True,verbose=1,validation_split=0.1)
+    
+    print(mymodel.summary())
+    print(history.history)
+    # TODO: check the accuracy
+    
+    # export the model as required in the assignment
+    json_string = mymodel.to_json()
+    with open('model.json', 'w') as outfile:
+        json.dump(json_string, outfile)
+        
+    # export the weights as required in the assignment
+    mymodel.save_weights('model.h5')
+
+
+    # test on other pictures
+    X_test = []
+    y_test = []
+    X_test,y_test = load_data(X_test,y_test,test_data_dir)
+    X_test = np.array(X_test).astype('float32')
+    y_test = np.array(y_test).astype('float32')
+    print('X_test.shape: ',X_test.shape)
+    print('Y_test.shape: ',y_test.shape)
+    
+    y_prediction = mymodel.predict(X_test,verbose=1)
+
+    print('predictions: ',y_prediction)
+    print('actual: ',y_test)
+  
+    # Explicitly reap session to avoid an AttributeError sometimes thrown by
+    # TensorFlow on shutdown. See:
+    # https://github.com/tensorflow/tensorflow/issues/3388
+    from keras import backend as K
+    K.clear_session()
+    
+        
